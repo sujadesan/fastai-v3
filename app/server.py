0ffad7b08f03872bef42ad1d2d442dc8f6722c87
sujadesan <56005760+sujadesan@users.noreply.github.com>
2019-10-01 22:59:20 +0530

    Update classes

--- app/server.py
+++ app/server.py
@@ -12,7 +12,7 @@ from starlette.staticfiles import StaticFiles
 export_file_url = 'https://github.com/sayakpaul/Malaria-Detection-with-Deep-Learning/blob/master/model/Malaria-Detection-Model.pkl'
 export_file_name = 'Malaria-Detection-Model.pkl'
 
-classes = ['Parasitized','Uninfected']
+classes = ['Parasitized', 'Uninfected']
 path = Path(__file__).parent
 
 app = Starlette()
